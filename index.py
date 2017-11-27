00f8d8a55c02063a482548b7b0d3b4006dde90c7
marekq <marekq>
2017-11-27 21:05:49 +0100

    render my very static homepage in HTML

--- index.py
+++ index.py
@@ -1,4 +1,30 @@
+import random
+
+rand = str(random.randint(1,2))
+html = """<html>
+<head><title>Marek Kuczy&#324;ski</title>
+<style type="text/css">
+body {
+    height: 50%;
+    background: #fff;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    padding: 0;
+    font-family: Courier New, Fixed;
+    font-size: 15px;
+    color: #555;
+}
+</style>
+
+</head><body>
+<center><h1><center>Marek Kuczy&#324;ski</h1>AWS solution architect
+<br><br>
+<img src="https://www.marek.rocks/images/"""+rand+""".jpg" width=100%>
+<br><br>
+<a target="_blank" href="https://github.com/marekq">github</a> | <a target="_blank" href="http://nl.linkedin.com/in/marekkuczynski">linkedin</a> | <a href="papers.html">university papers</a> | <a target="_blank" href="http://twitter.com/marekq">twitter</a><br><br>"""+str(rand)+"""</center>
+</body></html>"""
+
 def handler(event, context):
     return {'statusCode': 200,
-            'body': 'hello world',
+            'body': html,
             'headers': {'Content-Type': 'text/html'}}
