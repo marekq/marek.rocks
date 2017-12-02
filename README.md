90f3c917d8404dc449f87bf214d7eb71259b497c
Marek Kuczynski <marek.kuczynski@gmail.com>
2017-12-02 23:55:39 +0100

    few comments added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A very simple POC to demonstrate how static and dynamic pages can be made using AWS CodeStar; https://aws.amazon.com/codestar/
+
+Created by @marekq
--- index.py
+++ index.py
@@ -5,15 +5,17 @@
 
 import botocore.vendored.requests as requests, shutil, hashlib, os
 
-# retrieve the ip address of the visitor and return lat/lon 
+# retrieve the ip address of the visitor and return lat/lon from freegeoip.net
 def get_ip(event):
     ip  = str(event['headers']['X-Forwarded-For']).split(',')[0]
     a   = requests.get('https://freegeoip.net/csv/'+ip)
     b   = str(a.text).split(',')
+    # switch around the lat and lon
     c   = b[9]+','+b[8]
     d   = plot_gps(c)
     return str(d)
 
+# return a static blob of css
 def css():
     return """<style type="text/css">
     body {
@@ -28,13 +30,16 @@ def css():
     }
     </style>"""
 
-# create a 
+# create a map tile based on the visitor's gps coordinates
 def plot_gps(x):
     k   = os.environ['tomtomkey']
     u   = 'https://api.tomtom.com/map/1/staticimage?center='+str(x)+'&zoom=12&format=jpg&layer=basic&style=main&width=1280&height=250&view=Unified&key='+k
+
+    # create an md5 hash of the url path, this is unique per gps coordinate
     f   = str(hashlib.md5(u).hexdigest())
     g   = '/tmp/'+f+'.jpg'
 
+    # if there is no map tile on disk, download it from the tomtom api
     if not os.path.isfile(g):    
         r   = requests.get(u, stream = True)
         b   = 'file not found, downloaded'
@@ -46,9 +51,11 @@ def plot_gps(x):
     else:
         b = 'file '+g+' found in lambda cache'
 
+    # read the map tile and encode it as base64
     with open(g, 'rb') as x:                         
         data = x.read().encode("base64")
 
+    # parse the html file including the image
     h = '<html><head><title>Marek Kuczy&#324;ski</title>'+css()+'</head>'
     h += '<body><center><h1><center>Marek Kuczy&#324;ski</h1>AWS solution architect<br><br>'
     h += '<img src="data:image/gif;base64,'+data+'"><br><br>'
@@ -56,8 +63,7 @@ def plot_gps(x):
     h += '</center></body></html>'
     return h
 
-
-
+# return an html document when the lambda function is triggered
 def handler(event, context):
     return {'statusCode': 200,
             'body': get_ip(event),
