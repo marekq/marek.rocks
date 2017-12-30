364258cbc972c5ee7efde5a17ebcf08dc76fc7aa
marekq <marekq>
2017-12-30 19:27:43 +0100

    full redesign - included a live feed of the aws blog for demo purposes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-A very simple POC to demonstrate how static and dynamic pages can be made using AWS CodeStar; https://aws.amazon.com/codestar/
+marek.rocks
+===========
+
+The sourcecode of my website hosted at https://marek.rocks , which I use for demo's on how high performance, serverless webpages can be built using AWS Lambda.  
+
+The webpage is using an API gateway which forwards to Python Lambda code. The Lambda code automatically retrieves all of the AWS blog posts which are kept in a DynamoDB database. 
+
+![alt tag](https://raw.githubusercontent.com/marekq/marek.rocks/master/docs/1.png)
+
+
+You can find the Lambda code used to populate the DynamoDB here; https://github.com/marekq/rss-lambda
+
+
+Contact
+-------
+
+In case of questions or bugs, please raise an issue or reach out to @marekq!
--- index.py
+++ index.py
@@ -3,13 +3,43 @@
 # @marekq
 # www.marek.rocks
 
-import random
+import botocore.vendored.requests as requests, boto3, os, random, datetime, time
 
-def html():
-    i    = str(random.randint(1,2))
-    h    = """<html><head><title>Marek Kuczy&#324;ski</title>
+# get a random image back
+def get_image():
+    return '<img src="https://marek.rocks/images/'+str(random.randint(1,4))+'.jpg" width=100%>'
+  
+def get_date(x):
+    y = time.time()
+    z = int(y) - int(x)
     
-    <style type="text/css">
+    if z > 172800:
+        return 'posted '+str(int(z)/86400)+' days'
+    elif z > 86400:
+	    return 'posted '+str(int(z)/86400)+' day'
+    else:
+	    return 'posted '+str(int(z)/3600)+' hours'
+    
+# get all the blog posts from dynamodb    
+def get_dynamo():
+    d   = boto3.resource('dynamodb', region_name = os.environ['dynamo_region']).Table(os.environ['dynamo_table'])
+    h   = []
+    
+    for x in d.scan()['Items']:
+        if x.has_key('desc'):
+            h.append([x['timest'], x['title'], x['link'], x['desc']])
+        else:
+            h.append([x['timest'], x['title'], x['link'], 'no description found'])
+
+    y   = ''
+    for x in sorted(h, reverse = True):
+        y += '<b><a href='+x[2]+'>'+x[1]+'</a></b><br><i>posted '+get_date(x[0])+' ago</i><br><br>'+x[3]+'<br><br>'
+
+    return y
+    
+# return a static blob of css
+def css():
+    return """<style type="text/css">
     body {
         height: 50%;
         background: #fff;
@@ -20,15 +50,22 @@ def html():
         font-size: 15px;
         color: #555;
     }
-    </style>
-    
-    </head><body><center><h1><center>Marek Kuczy&#324;ski</h1>AWS solution architect<br><br>
-    <img src="https://www.marek.rocks/images/"""+i+""".jpg" width=100%><br><br>
-    <a target="_blank" href="https://github.com/marekq">github</a> | <a target="_blank" href="http://nl.linkedin.com/in/marekkuczynski">linkedin</a> | <a href="papers.html">university papers</a> | <a target="_blank" href="http://twitter.com/marekq">twitter</a><br><br></center>
-    </body></html>"""
+    </style>"""
+
+# parse the html file including the image
+def parse_html():
+    h = '<html><head><title>Marek Kuczy&#324;ski</title>'+css()+'</head>'
+    h += '<body><center><h1><center>Marek Kuczy&#324;ski</h1>AWS solution architect<br><br>'
+    h += '<a href="https://github.com/marekq/marek.rocks"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>'
+    #h += get_image()+'<br><br>'
+    h += '<a target="_blank" href="https://github.com/marekq">github</a> | <a target="_blank" href="http://nl.linkedin.com/in/marekkuczynski">linkedin</a> | <a href="https://s3-eu-west-1.amazonaws.com/marek.rocks/papers.html">university papers</a> | <a target="_blank" href="http://twitter.com/marekq">twitter</a><br><br>'
+    h += '<h3><a href="https://github.com/marekq/rss-lambda">AWS blog feeds</a></h3></center>'
+    h += get_dynamo()
+    h += '</body></html>'
     return h
 
+# return an html document when the lambda function is triggered
 def handler(event, context):
     return {'statusCode': 200,
-            'body': html(),
+            'body': parse_html(),
             'headers': {'Content-Type': 'text/html'}}
