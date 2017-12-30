b679dbca8faa3531fe7e12f34b13e5f72becb488
marekq <marekq>
2017-12-31 00:32:51 +0100

    speed up dynamo sessions, added userstat tracing

--- lambda.py
+++ lambda.py
@@ -3,11 +3,15 @@
 # @marekq
 # www.marek.rocks
 
-import botocore.vendored.requests as requests, boto3, os, random, datetime, time
+import botocore.vendored.requests as requests, boto3, datetime, os, random, time
 
 # get a random image back
 def get_image():
     return '<img src="https://s3-'+os.environ['s3_region']+'.amazonaws.com/'+os.environ['s3_bucket']+'/images/'+str(random.randint(1,4))+'.jpg" width=100%>'
+  
+def get_dynamo_sess(): 
+    d   = boto3.resource('dynamodb', region_name = os.environ['dynamo_region'])
+    return d
   
 def get_date(x):
     y = time.time()
@@ -21,10 +25,9 @@ def get_date(x):
         return 'posted '+str(int(z)/3600)+' hours'
     
 # get all the blog posts from dynamodb    
-def get_dynamo():
-    d   = boto3.resource('dynamodb', region_name = os.environ['dynamo_region']).Table(os.environ['dynamo_table'])
+def get_posts():
     h   = []
-    
+    d   = get_dynamo_sess().Table(os.environ['dynamo_post_table'])
     for x in d.scan()['Items']:
         if x.has_key('desc'):
             h.append([x['timest'], x['title'], x['link'], x['desc']])
@@ -37,6 +40,16 @@ def get_dynamo():
 
     return y
 
+def write_dynamo(ip, co, ua):
+    d   = get_dynamo_sess().Table(os.environ['dynamo_user_table'])
+    d.put_item(Item = {
+        'unixts' : datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 
+        'timest' : str(int(time.time())),
+        'ip' : ip,
+        'usera' : ua,
+        'country' : co
+    })
+    
 # parse the html file including the image
 def parse_html():
     h = '<html><head><title>Marek Kuczy&#324;ski</title><link rel="stylesheet" type="text/css" href="https://s3-'+os.environ['s3_region']+'.amazonaws.com/'+os.environ['s3_bucket']+'/main.css"></script></head>'
@@ -46,13 +59,18 @@ def parse_html():
     h += '<a target="_blank" href="http://nl.linkedin.com/in/marekkuczynski">linkedin</a> | '
     h += '<a href="https://s3-'+os.environ['s3_region']+'.amazonaws.com/'+os.environ['s3_bucket']+'/papers.html">university papers</a> | '
     h += '<a target="_blank" href="http://twitter.com/marekq">twitter</a><br><br>'
-    h += '<h3>AWS blog feeds</h3></center>'
-    h += get_dynamo()
-    h += '</body></html>'
+    h += '<h3>AWS blog feeds</h3><table width="800px"><tr><td>'
+    h += get_posts()
+    h += '</td></tr></table></center></body></html>'
     return h
 
 # return an html document when the lambda function is triggered
 def handler(event, context):
+    ip = str(event['headers']['X-Forwarded-For']).split(',')[0]
+    co = str(event['headers']['CloudFront-Viewer-Country'])
+    ua = str(event['headers']['User-Agent'])
+    write_dynamo(ip, co, ua)
+
     return {'statusCode': 200,
             'body': parse_html(),
             'headers': {'Content-Type': 'text/html'}}
