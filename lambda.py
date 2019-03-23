033ea808a2f43909e4b0ddf968225c672ff25431
marekq <marek.kuczynski@gmail.com>
2019-03-23 20:58:51 +0100

    small fixes from last months

--- lambda.py
+++ lambda.py
@@ -6,7 +6,8 @@
 
 # set import path for python libraries to the './libs' folder.
 
-import boto3, datetime, epsagon, os, time, sys
+import boto3, os, time, sys
+from datetime import datetime, timedelta
 from boto3.dynamodb.conditions import Key, Attr
 sys.path.insert(0, './libs')
 
@@ -15,9 +16,12 @@ from aws_xray_sdk.core import patch_all
 
 patch_all()
 
+
 # defines all blog categories from dynamodb which will be included on the page
-blogs       = ['all', 'whats-new', 'newsblog', 'big-data', 'security', 'mobile', 'architecture', 'compute', 'database', 'management-tools', 'security-bulletins', 'public-sector', 'gamedev', 'ml', 'serverless'] # ['cli']
+blogs       = {'all':'all', 'whats-new': 'new', 'newsblog': 'new', 'big-data': 'bigd', 'security':'sec', 'mobile':'mob', 'compute':'cmp', 'database': 'dbs', 'management-tools': 'mgt', 'security-bulletins': 'sec', 'public-sector': 'pub', 'gamedev': 'gam', 'ml': 'ml', 'serverless': 'src', 'architecture': 'arc', 'cli': 'cli', 'devops': 'dev', 'java': 'jav'} 
+
 baseurl     = 'https://marek.rocks/'
+c			= ''
 
 # open a session with the dynamodb service
 def get_dynamo_sess(): 
@@ -31,6 +35,8 @@ def get_date(x):
 	y		= time.time()
 	z		= int(y) - int(x)
 	
+	years	= str(int(int(z)/31536000))
+	months	= str(int(int(z)/2592000))
 	weeks	= str(int(int(z)/604800))
 	days	= str(int(int(z)/86400))
 	hours	= str(int(int(z)/3600) % 24)
@@ -42,20 +48,28 @@ def get_date(x):
 	elif days == str('0') and hours == str('0'):
 		return mins+'m'
 	
-	elif int(days) < int('99'):
+	elif int(days) < int(31):
 		return days+'d'
 
+	elif int(days) < int(365):
+		return weeks+'w'
+
 	else:
-		return '99+d'
+		return years+'y'
 
 # get all the blog posts from dynamodb    
 def get_posts(d, npa, tag, url):
 	y   = ''
 	h   = []
 	
+	# get timestamps for 90 days ago
+	n	= datetime.now()
+	s	= n - timedelta(days = 90)
+	ts	= int(time.mktime(s.timetuple()))
+
 	# check if a url path was specified and return all articles. if no path was selected, return all articles. return the last 90 days of blogposts only.
-	if npa == 'all' or npa == 'mini':
-		e	= d.query(IndexName = 'allts', KeyConditionExpression = Key('allts').eq('y'))
+	if npa == 'all':
+		e	= d.query(IndexName = 'allts', KeyConditionExpression = Key('allts').eq('y'), FilterExpression= Key('timest').gt(str(ts)))
 		for x in e['Items']:
 			h.append([x['timest'], x['title'], x['link'], x['desc'], x['source'], x['author'], x['tag']])
 
@@ -68,45 +82,43 @@ def get_posts(d, npa, tag, url):
 	# else resume the articles for the tag specified for the last 90 days
 	else:
 		# calculate the unix timestamp for 90 days ago
-		n	= datetime.datetime.now()
-		s	= n - datetime.timedelta(days = 90)
-		ts	= int(time.mktime(s.timetuple()))
+		if npa in ['ml', 'big-data', 'public-sector', 'security', 'whats-new', 'newsblog']:
+			ts	= int(time.mktime(s.timetuple()))
+			
+		else:
+			ts	= int(1)
 		
 		e   = d.query(KeyConditionExpression = Key('source').eq(npa) & Key('timest').gt(str(ts)))
 		for x in e['Items']:
 			h.append([x['timest'], x['title'], x['link'], x['desc'], x['source'], x['author'], x['tag']])
 
 	# print all the articles in html, shorten description text if needed
-	if npa != 'mini':
+	for x in sorted(h, reverse = True):
+		desc    	=	x[3]
+		t           =	get_date(x[0])
 
-		for x in sorted(h, reverse = True):
-			desc    	= x[3]
-			t           = get_date(x[0])
-			y			+= '<tr><td width = 7%, valign = "top", align = "right"><font color = "#D3D3D3">'+t+'</font></td><td><button class="collapsible">'+x[1]+'</button><div class="content"><p>'+desc+'<br><br><a href="'+x[2].strip()+'">visit article here</a></p></div></td></tr>\n'
+		y			+= '<tr><td valign = "top", align = "center", id = "time"><font color = "red">'+t+'</font></td>'
+		if npa == 'all':
+			y		+= '<td valign = "top", align = "center", id = "short">'+blogs[x[4]].upper()+'</td>'
 
-	else:
-		ds	= ''
-		for x in sorted(h, reverse = True):
-			t           = get_date(x[0])
-			if t != ds:
-				ds		= t
-				y		+= '<b>posted '+t+'</b>'
-			y += x[4]+'<a href='+baseurl+x[2].strip()+' target="_blank">'+x[1][:70]+'</a><br>\n'
+		y			+= '<td><button class="collapsible">'+x[1]+'</button><div class="content"><p id = "desc"><i>posted by '+x[5]+' in '+x[4]+' on '+str(datetime.fromtimestamp(int(x[0])))+'</i><br><br>'+desc+'<br><br><a target="_blank", href="'+x[2].strip()+'">visit article here</a></p></div></td></tr>\n'
 
-	return y
+	c	= int(len(h))
+
+	return y, c
 
 # generate highlighted url for aws blog links
 def generate_urls(d, npa, url):
 	h   = '<center>'
 	
-	for x in blogs:
+	for x in blogs.keys():
 		if x == npa:
 			h += '<a href="'+url+str(x)+'"><font color = "red">'+x+'</font></a> &#8226; '
 	
 		else:
 			h += '<a href="'+url+str(x)+'">'+x+'</a> &#8226; '
 			
-	return h+'<a href="#about">about this page</a></center>'
+	return h[:-5]
 
 # print http headers for debug headers
 def parse_debug(event):
@@ -118,14 +130,11 @@ def check_path(x):
 	if x.startswith('/http://') or x.startswith('/https://'):
 		return 'redir', x[1:]
 
-	if x.strip('/') in blogs:
+	if x.strip('/') in blogs.keys():
 		return x.strip('/'), ''
 	
 	elif x.strip('/')[:3] == 'tag':
 		return 'tag', x.strip('/')[4:]
-
-	elif x.strip('/') == 'mini':
-		return 'mini', ''
 	
 	else:
 		return 'all', ''
@@ -146,15 +155,25 @@ def check_ua(x):
 
 	return b
 
+def counter(url, co):
+	if 'marek' not in url:
+		return str(co)
+	else:
+		return ''
+
 # parse the html file including the image
 def parse_html(d, npa, tag, url):
-	h	=  '<html><head><meta charset="UTF-8"><title>marek\'s serverless demo</title>'
-	h	+= load_file('main.css')+'</head>'
-	h	+= '<body><center><table width="800px"><tr><td><h1><center id = "top">Marek\'s Serverless AWS blog</center></h1></td></tr>'
-	h	+= '<tr><td>'+generate_urls(d, npa, url)+'</td></tr></table><br>'
-	h	+= '<table width = "800px"><tr><td>'
-	h	+= get_posts(d, npa, tag, url)
-	h	+= '</tr></td></table>'+load_file('collapse.js')+'</body></html>'
+	po, co	= get_posts(d, npa, tag, url) 	
+	xray_recorder.current_subsegment().put_annotation('post-count', co)
+
+	h		=  '<html><head><meta charset="UTF-8"><title>marek\'s serverless demo</title>'
+	h		+= load_file('main.css')+'</head>'
+	h		+= '<body><center>'+counter(url, co)
+	h		+= '<table width = 800px><tr><td><h1><center id = "top">marek\'s serverless AWS blog</center></h1></td></tr>'
+	h		+= '<tr><td id = "urls">'+generate_urls(d, npa, url)+'</td></tr></table><br>'
+	h		+= '<table>'
+	h		+= po
+	h		+= '</table>'+load_file('collapse.js')+' '+c+'</body></html>'
 	return h
 
 # return an html document when the lambda function is triggered
@@ -164,9 +183,9 @@ def handler(event, context):
 	seg     = xray_recorder.begin_subsegment('dynamo-session')
 	ip      = str(event['headers']['X-Forwarded-For']).split(',')[0]
 	ua      = str(event['headers']['User-Agent'])
+	print('IP: '+str(event['headers']['X-Forwarded-For']))
 
 	# print request headers in cloudwatch for debug purposes
-	#print('%%%', str(event['headers']))
 	xray_recorder.end_subsegment()
 
 	# clean the given url path and print debug
@@ -194,13 +213,13 @@ def handler(event, context):
 	# else parse the html page
 	else:
 		url     = check_ua(ua)
-		h       = parse_html(d, npa, tag, url)
+		html    = parse_html(d, npa, tag, url)
 	
 		print('200 ***', str(event['headers']['User-Agent']), str(event['headers']['Host']), pa, url, tag)
 		xray_recorder.current_subsegment().put_annotation('statuscode', '200')
 
 		return {'statusCode': '200',
-				'body': str(h),
+				'body': str(html),
 				'headers': {'Content-Type': 'text/html', 'charset': 'utf-8'}} 
 
 	xray_recorder.end_subsegment()
